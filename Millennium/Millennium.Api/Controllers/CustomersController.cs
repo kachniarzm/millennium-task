1815fd87e225ea0856bb22439a0ec718b1d7faa6
Maciej Kachniarz <maciej.kachniarz@gmail.com>
2025-03-13 10:41:22 +0100

    refactoring

--- Millennium/Millennium.Api/Controllers/CustomersController.cs
+++ Millennium/Millennium.Api/Controllers/CustomersController.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Millennium.Api.Models;
 using Millennium.Api.Repositories;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using System.ComponentModel.DataAnnotations;
 
 namespace Millennium.Api.Controllers;
 
@@ -11,7 +10,7 @@ namespace Millennium.Api.Controllers;
 public class CustomersController : ControllerBase
 {
     private readonly ILogger<CustomersController> _logger;
-    private ICustomersRepository _customersRepository;
+    private readonly ICustomersRepository _customersRepository;
 
     public CustomersController(ILogger<CustomersController> logger, ICustomersRepository customersRepository)
     {
@@ -19,78 +18,88 @@ public class CustomersController : ControllerBase
         _customersRepository = customersRepository;
     }
 
-    // GET: api/<CustomersController>
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public ActionResult<List<Customer>> Get()
+    public ActionResult<IEnumerable<Customer>> Get()
     {
-        return _customersRepository.GetAll();
+        var customers = _customersRepository.GetAll();
+        return Ok(customers);
     }
 
-    // GET api/<CustomersController>/5
     [HttpGet("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
     public ActionResult<Customer> Get(int id)
     {
-        var customer = _customersRepository.GetById(id);
+        if (id <= 0)
+        {
+            return BadRequest("Invalid customer ID.");
+        }
 
+        var customer = _customersRepository.GetById(id);
         if (customer == null)
         {
-            return NotFound($"Customer with id {id} does not exist.");
+            return NotFound($"Customer with ID {id} does not exist.");
+        }
+        return Ok(customer);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+    public ActionResult<Customer> Post([FromBody, Required] CustomerAddEdit customer)
+    {
+        if (customer == null)
+        {
+            return BadRequest("Customer data is required.");
         }
 
-        return customer;
+        var createdCustomer = _customersRepository.Create(customer);
+        _logger.LogInformation($"Customer with ID {createdCustomer.Id} created.");
+        return CreatedAtAction(nameof(Get), new { id = createdCustomer.Id }, createdCustomer);
     }
 
-    // POST api/<CustomersController>
-    [HttpPost]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public ActionResult<Customer> Post([FromBody] CustomerAddEdit customer)
-    {
-        var created = _customersRepository.Create(customer);
-
-        _logger.LogInformation($"Customer with id {created.Id} created.");
-        return created;
-    }
-
-    // PUT api/<CustomersController>/5
     [HttpPut("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public ActionResult<Customer> Put(int id, [FromBody] CustomerAddEdit customer)
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+    public ActionResult<Customer> Put(int id, [FromBody, Required] CustomerAddEdit customer)
     {
-        var updated = _customersRepository.Update(id, customer);
-
-        if (updated == null)
+        if (id <= 0 || customer == null)
         {
-            var message = $"Customer with id {id} not found. Cannot be updated.";
-            _logger.LogInformation(message);
+            return BadRequest("Invalid request parameters.");
+        }
+
+        var updatedCustomer = _customersRepository.Update(id, customer);
+        if (updatedCustomer == null)
+        {
+            var message = $"Customer with ID {id} not found for update.";
+            _logger.LogWarning(message);
             return NotFound(message);
         }
 
-        _logger.LogInformation($"Customer with id {id} updated.");
-        return updated;
+        _logger.LogInformation($"Customer with ID {id} updated.");
+        return Ok(updatedCustomer);
     }
 
-    // DELETE api/<CustomersController>/5
     [HttpDelete("{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public ActionResult Delete(int id)
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+    public IActionResult Delete(int id)
     {
-        if (_customersRepository.Delete(id))
+        if (id <= 0)
         {
-            _logger.LogInformation($"Customer with id {id} deleted.");
-            return NoContent();
+            return BadRequest("Invalid customer ID.");
         }
-        else
+
+        if (!_customersRepository.Delete(id))
         {
-            var message = $"Customer with id {id} not found. Cannot be deleted.";
-            _logger.LogInformation(message);
+            var message = $"Customer with ID {id} not found for deletion.";
+            _logger.LogWarning(message);
             return NotFound(message);
         }
+
+        _logger.LogInformation($"Customer with ID {id} deleted.");
+        return NoContent();
     }
 }
--- Millennium/Millennium.UnitTests/CustomersControllerUnitTests.cs
+++ Millennium/Millennium.UnitTests/CustomersControllerUnitTests.cs
@@ -20,7 +20,11 @@ internal class CustomersControllerUnitTests
 
         var result = controller.Get();
 
-        Assert.That(result?.Value?.Count() == 3);
+        var okResult = result.Result as OkObjectResult;
+        Assert.That(okResult, Is.Not.Null);
+        var customers = okResult.Value as IEnumerable<Customer>;
+        Assert.That(customers, Is.Not.Null); 
+        Assert.That(customers.Count(), Is.EqualTo(3));
     }
 
     [Test]
@@ -45,9 +49,13 @@ internal class CustomersControllerUnitTests
 
         var result = controller.Get(1);
 
-        Assert.That(result?.Value?.Id == 1);
-        Assert.That(result?.Value?.FirstName == "Delbert");
-        Assert.That(result?.Value?.LastName == "Traver");
+        var okResult = result.Result as OkObjectResult;
+        Assert.That(okResult, Is.Not.Null, "Expected OkObjectResult but got null");
+        var customer = okResult.Value as Customer;
+        Assert.That(customer, Is.Not.Null, "Expected Customer object but got null");
+        Assert.That(customer.Id, Is.EqualTo(1));
+        Assert.That(customer.FirstName, Is.EqualTo("Delbert"));
+        Assert.That(customer.LastName, Is.EqualTo("Traver"));
     }
 
     [Test]
@@ -60,9 +68,13 @@ internal class CustomersControllerUnitTests
 
         var result = controller.Post(new CustomerAddEdit());
 
-        Assert.That(result?.Value?.Id == 1);
-        Assert.That(result?.Value?.FirstName == "Delbert");
-        Assert.That(result?.Value?.LastName == "Traver");
+        var createdAtResult = result.Result as CreatedAtActionResult;
+        Assert.That(createdAtResult, Is.Not.Null, "Expected CreatedAtActionResult but got null");
+        var customer = createdAtResult.Value as Customer;
+        Assert.That(customer, Is.Not.Null, "Expected Customer object but got null");
+        Assert.That(customer.Id, Is.EqualTo(1));
+        Assert.That(customer.FirstName, Is.EqualTo("Delbert"));
+        Assert.That(customer.LastName, Is.EqualTo("Traver"));
     }
 
     [Test]
@@ -87,9 +99,13 @@ internal class CustomersControllerUnitTests
 
         var result = controller.Put(1, new CustomerAddEdit());
 
-        Assert.That(result?.Value?.Id == 1);
-        Assert.That(result?.Value?.FirstName == "Delbert");
-        Assert.That(result?.Value?.LastName == "Traver");
+        var okResult = result.Result as OkObjectResult;
+        Assert.That(okResult, Is.Not.Null, "Expected OkObjectResult but got null");
+        var customer = okResult.Value as Customer;
+        Assert.That(customer, Is.Not.Null, "Expected Customer object but got null");
+        Assert.That(customer.Id, Is.EqualTo(1));
+        Assert.That(customer.FirstName, Is.EqualTo("Delbert"));
+        Assert.That(customer.LastName, Is.EqualTo("Traver"));
     }
 
     [Test]
